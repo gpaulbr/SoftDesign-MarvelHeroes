da7a099e46b29c850eb0da86af447ef66a6e2ddb
gpaulbr <gpaulbr@gmail.com>
2019-02-05 01:37:39 -0200

    add storyboardInstantiable protocol and ImageViewExtension

---
+++ MarvelHeroes/Helpers/UIImageViewExtension.swift
@@ -0,0 +1,30 @@
+//
+//  UIImageViewExtension.swift
+//  MarvelHeroes
+//
+//  Created by Gabriel Paul on 05/02/19.
+//  Copyright © 2019 gp. All rights reserved.
+//
+
+import UIKit
+
+extension UIImageView {
+    func dowloadFromServer(url: URL, contentMode mode: UIView.ContentMode = .scaleAspectFit) {
+        contentMode = mode
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            guard
+                let httpURLResponse = response as? HTTPURLResponse, httpURLResponse.statusCode == 200,
+                let mimeType = response?.mimeType, mimeType.hasPrefix("image"),
+                let data = data, error == nil,
+                let image = UIImage(data: data)
+                else { return }
+            DispatchQueue.main.async() {
+                self.image = image
+            }
+            }.resume()
+    }
+    func dowloadFromServer(link: String, contentMode mode: UIView.ContentMode = .scaleAspectFit) {
+        guard let url = URL(string: link) else { return }
+        dowloadFromServer(url: url, contentMode: mode)
+    }
+}
