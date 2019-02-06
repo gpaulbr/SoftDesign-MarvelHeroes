1825459b50b39b07d9238cd2d3493718724dcac7
gpaulbr <gpaulbr@gmail.com>
2019-02-06 08:20:55 -0200

    add heroDetails coordinator

---
+++ MarvelHeroes/Coordinators/HeroDetailsCoordinator.swift
@@ -0,0 +1,33 @@
+//
+//  HeroDetailsCoordinator.swift
+//  MarvelHeroes
+//
+//  Created by Gabriel Paul on 06/02/19.
+//  Copyright © 2019 gp. All rights reserved.
+//
+
+import UIKit
+
+class HeroDetailsCoordinator: Coordinator {
+    
+    fileprivate let presenter: UINavigationController
+    var childrenCoordinators: [Coordinator]
+    
+    fileprivate let hero: Hero
+    fileprivate var heroDetailsViewController: HeroDetailsViewController?
+    
+    init(presenter: UINavigationController, hero: Hero) {
+        self.presenter = presenter
+        self.hero = hero
+        self.childrenCoordinators = []
+    }
+    
+    func start() {
+        let heroDetailsViewController = HeroDetailsViewController.instantiate()
+        let heroDetailsViewModel = HeroDetailsViewModel(hero: hero)
+        heroDetailsViewController.viewModel = heroDetailsViewModel
+        
+        self.heroDetailsViewController = heroDetailsViewController
+        presenter.pushViewController(heroDetailsViewController, animated: true)
+    }
+}
